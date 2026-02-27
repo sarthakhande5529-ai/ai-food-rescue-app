b542ebf773837586420b1ae033d2c409781e147b
sarthakhande5529-ai <sarthakhande5529@gmail.com>
2026-02-27 16:45:10 +0530

    Update and rename App.jsx to app.jsx

--- src/src/App.jsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <div style={{ padding: 40, fontFamily: "Arial" }}>
-      <h1>AI Food Rescue 🚀</h1>
-      <p>Your new clean project is ready.</p>
-    </div>
-  )
-}
